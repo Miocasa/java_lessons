dc6a969b1bca90e0b82805dae59a099dc9b40a92
Miocasa <narimanabdualiev06@gmail.com>
2026-09-19 13:42:40 +0500

    4.2 high №1 counter fix

--- src/Main.java
+++ src/Main.java
@@ -324,13 +324,11 @@ public class Main{
                     double x0 = ((x1 * x1 + y1 * y1) * (y2 - y3) + (x2 * x2 + y2 * y2) * (y3 - y1) + (x3 * x3 + y3 * y3) * (y1 - y2)) / d;
                     double y0 = ((x1 * x1 + y1 * y1) * (x3 - x2) + (x2 * x2 + y2 * y2) * (x1 - x3) + (x3 * x3 + y3 * y3) * (x2 - x1)) / d;
 
-                    double r = Math.hypot(x1 - x0, y1 - y0);
+                    double r = hypot(x1 - x0, y1 - y0);
 
                     int count = 0;
                     for (int m = 0; m < n; m++) {
-                        double dist = Math.hypot(points[m][0] - x0, points[m][1] - y0);
-                        count++;
-
+                        if(abs(hypot(points[m][0] - x0, points[m][1] - y0) - r) < Double.MIN_NORMAL) count++;
                     }
 
                     if (count > maxCount) {
